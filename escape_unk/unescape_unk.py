ce5fe99fe576b1b8ae8ef66c7d0ae25d0173a44f
ZJaume <jzaragoza@prompsit.com>
2022-09-28 10:17:51 +0000

    Fix unescape entry point

--- escape_unk/unescape_unk.py
+++ escape_unk/unescape_unk.py
@@ -7,17 +7,21 @@ def unescape(match):
     ''' Convert hex values to unicode string '''
     return bytes.fromhex((match.captures()[0].strip('[]'))).decode('utf-8')
 
-for line in sys.stdin:
-    # Find splits and matches inside the sentence
-    escaped = list(escaped_re.finditer(line.strip()))
-    splits = list(escaped_re.splititer(line.strip()))
-    output = ''
+def main():
+    for line in sys.stdin:
+        # Find splits and matches inside the sentence
+        escaped = list(escaped_re.finditer(line.strip()))
+        splits = list(escaped_re.splititer(line.strip()))
+        output = ''
 
-    # Join splits with unescaped matches
-    for i, split in enumerate(splits):
-        if i != len(splits)-1:
-            output += split + unescape(escaped[i])
-        else:
-            output += split
+        # Join splits with unescaped matches
+        for i, split in enumerate(splits):
+            if i != len(splits)-1:
+                output += split + unescape(escaped[i])
+            else:
+                output += split
 
-    print(output)
+        print(output)
+
+if __name__ == "__main__":
+    main()
